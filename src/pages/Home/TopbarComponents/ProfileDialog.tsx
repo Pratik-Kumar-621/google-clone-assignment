c362243596f2c7a2c0f223f4b2efc51a3d81b122
Pratik Kumar <70286186+Pratik-Kumar-621@users.noreply.github.com>
2025-04-10 04:09:10 +0530

    profile dialog style

--- src/pages/Home/TopbarComponents/ProfileDialog.tsx
+++ src/pages/Home/TopbarComponents/ProfileDialog.tsx
@@ -1,9 +1,10 @@
-import { Dialog, Button, Slide, Drawer } from "@mui/material";
+import { Dialog, Button, Slide, Drawer, IconButton } from "@mui/material";
 import { TransitionProps } from "@mui/material/transitions";
 import React, { useState } from "react";
 import { dummyAccounts } from "../../../data/data";
 import { dummyAccountsType } from "../../../data/types";
 import ExpandCircleDownOutlinedIcon from "@mui/icons-material/ExpandCircleDownOutlined";
+import CloseIcon from "@mui/icons-material/Close";
 import { accountSettingLinks } from "../homeData";
 const Transition = React.forwardRef(function Transition(
   props: TransitionProps & {
@@ -43,89 +44,122 @@ const ProfileDialog = (props: dialogProps) => {
       onClose={handleClose}
       aria-describedby="alert-dialog-slide-description"
       fullWidth
+      className="dialog"
+      sx={{ background: "rgba(255, 255, 255, 0.072)" }}
     >
-      <div className="dialog-content">
-        <div className="dialog-content-heading">Google</div>
-        <div className="dialog-content-details">
-          <div className="dialog-content-details-user">
-            <div className="dialog-content-details-user-info">
+      <div className="dialogprofile">
+        <div className="dialogprofile-close">
+          <IconButton onClick={handleClose}>
+            <CloseIcon />
+          </IconButton>
+        </div>
+        <div className="dialogprofile-heading">Google</div>
+        <div className="dialogprofile-details">
+          <div className="dialogprofile-details-user">
+            <div className="dialogprofile-details-user-info">
               <img
-                className="dialog-content-details-user-info-image"
+                className="dialogprofile-details-user-info-image"
                 src={userDetails.image}
                 alt={userDetails.name}
               />
-              <div className="dialog-content-details-user-info-name">
-                {userDetails.name}
-              </div>
-              <div className="dialog-content-details-user-info-email">
-                {userDetails.email}
+              <div className="dialogprofile-details-user-info-details">
+                <div className="dialogprofile-details-user-info-name">
+                  {userDetails.name}
+                </div>
+                <div className="dialogprofile-details-user-info-email">
+                  {userDetails.email}
+                </div>
               </div>
             </div>
 
             <div
-              className="dialog-content-details-user-button"
+              className="dialogprofile-details-user-button"
               onClick={() => setShowAccounts(!showAccounts)}
             >
               <ExpandCircleDownOutlinedIcon />
             </div>
           </div>
-          <Button
-            variant="outlined"
-            className="dialog-content-details-button"
-            onClick={() =>
-              handleDrawerOpen({
-                text: "Manage your Google Account",
-                description:
-                  "Manage your Google Account settings, privacy options, and personalization preferences.",
-              })
-            }
-          >
-            Manage your Google Account
-          </Button>
+          <div className="dialogprofile-details-button">
+            <Button
+              variant="outlined"
+              className="dialogprofile-details-button"
+              onClick={() =>
+                handleDrawerOpen({
+                  text: "Manage your Google Account",
+                  description:
+                    "Manage your Google Account settings, privacy options, and personalization preferences.",
+                })
+              }
+            >
+              Manage your Google Account
+            </Button>
+          </div>
           <div
-            className={`dialog-content-details-accounts ${
+            className={`dialogprofile-details-accounts ${
               showAccounts ? "accounts-show" : "accounts-hide"
             }`}
           >
             {dummyAccounts.map((item: dummyAccountsType) => {
               return (
-                <div className="dialog-content-details-accounts-item">
-                  <div className="dialog-content-details-accounts-item-image">
+                <div className="dialogprofile-details-accounts-item">
+                  <div className="dialogprofile-details-accounts-item-image">
                     {item.image}
                   </div>
-                  <div className="dialog-content-details-accounts-item-name">
-                    {item.name}
-                  </div>
-                  <div className="dialog-content-details-accounts-item-email">
-                    {item.email}
+                  <div>
+                    <div className="dialogprofile-details-accounts-item-name">
+                      {item.name}
+                    </div>
+                    <div className="dialogprofile-details-accounts-item-email">
+                      {item.email}
+                    </div>
                   </div>
                 </div>
               );
             })}
           </div>
         </div>
-        <div className="dialog-content-links">
+
+        <div className="dialogprofile-links">
           {accountSettingLinks.map((item: any) => {
             return (
               <div
-                className="dialog-content-links-item"
+                className="dialogprofile-links-item"
                 key={item.id}
                 onClick={() => handleDrawerOpen(item)}
               >
-                <div className="dialog-content-links-item-icon">
+                <div className="dialogprofile-links-item-icon">
                   {item.children}
                 </div>
-                <div className="dialog-content-links-item-name">
-                  {item.text}
-                </div>
+                <div className="dialogprofile-links-item-name">{item.text}</div>
               </div>
             );
           })}
         </div>
-        <div className="dialog-content-footer">
-          <div className="dialog-content-footer-item">Privacy Policy</div>
+
+        <div className="dialogprofile-footer">
+          <div
+            className="dialogprofile-footer-item"
+            onClick={() =>
+              handleDrawerOpen({
+                text: "Privacy Policy",
+                description: "Learn about how we handle your data and privacy.",
+              })
+            }
+          >
+            Privacy Policy
+          </div>
           <div className="seperator-footer"></div>
-          <div className="dialog-content-footer-item">Terms and Conditions</div>
+          <div
+            className="dialogprofile-footer-item"
+            onClick={() =>
+              handleDrawerOpen({
+                text: "Terms and Conditions",
+                description: "Review our terms and conditions.",
+              })
+            }
+          >
+            Terms and Conditions
+          </div>
         </div>
       </div>
       <Drawer
